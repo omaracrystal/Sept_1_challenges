109e649c589c86a8cefbca78f9e3a6eee4ed7f97
omaracrystal <omaracrystal@gmail.com>
2015-09-01 13:53:23 -0600

    fixed flaws in jasmine test - second test still fails

--- spec/support/spec.js
+++ spec/support/spec.js
@@ -1,7 +1,7 @@
 var code = require('../../secondGreatLow.js');
 
 var test1 = [7, 7, 12, 98, 106]
-var test2 = [5,23, -112, 6, 70, 70, -112]
+var test2 = [5, 23, -112, 6, 70, 70, -112]
 var test3 = [-22, 22]
 var test4 = [10, 89, 3]
 
@@ -13,15 +13,15 @@ describe("secondGreatLow1 function", function() {
   });
 
   it("Detects duplicates and negative numbers and outputs 2nd Min and 2nd Max", function() {
-     expect(code.secondGreatLow2(test2)).toEqual([5, 23]);
+     expect(code.secondGreatLow1(test2)).toEqual([5, 23]);
   });
 
   it("Takes an array with only 2 values and outputs 2nd Max and 2nd Min", function() {
-     expect(code.secondGreatLow2(test3)).toEqual([22, -22]);
+     expect(code.secondGreatLow1(test3)).toEqual([22, -22]);
   });
 
    it("Takes an array with only 3 values and outputs middle number which is the 2nd Max and 2nd Min", function() {
-     expect(code.secondGreatLow2(test4)).toEqual([89, 89]);
+     expect(code.secondGreatLow1(test4)).toEqual([10, 10]);
   });
 
 });
@@ -44,7 +44,7 @@ describe("secondGreatLow2 function", function() {
   });
 
    it("Takes an array with only 3 values and outputs middle number which is the 2nd Max and 2nd Min", function() {
-     expect(code.secondGreatLow2(test4)).toEqual("89 89");
+     expect(code.secondGreatLow2(test4)).toEqual("10 10");
   });
 
 });
